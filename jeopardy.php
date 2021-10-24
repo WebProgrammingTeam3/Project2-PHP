1c54145380a162047bd1f065b2485344e6772696
Your Name <you@example.com>
2021-10-24 18:01:24 -0400

    HTML and CSS adjustments to jeopardy page

--- jeopardy.php
+++ jeopardy.php
@@ -16,38 +16,42 @@ if($_SESSION['UserData']['Username'] == "team1"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel = "stylesheet"    type = "text/css"  href = "J2.css" />
+    <link rel = "stylesheet"    type = "text/css"  href = "j2.css" />
     <title>Jeopardy</title>
 </head>
 
+
 <body>
-<table align ="center" border =1" width=600" height="450">
-    <tr>
-        <td>Food</td><td>Buildings</td><td>Comp Sci</td><td>Parking</td><td>Other Degrees</td>
-    </tr>
-    <tr>
-        <td><a id = "value" href="food1.html">$100</a></td><td><a id = "value" href="buildings1.html">$100</a></td><td><a id = "value" href="compSci1.html">$100</a></td><td><a id = "value" href="parking1.html">$100</a></td><td><a id = "value" href="otherDegrees1.html">$100</a></td>
-    </tr>
-    <tr>
-        <td><a id = "value" href="food2.html">$200</a></td><td><a id = "value" href="buildings2.html">$200</a></td><td><a id = "value" href="compSci2.html">$200</a></td><td><a id = "value" href="parking2.html">$200</a></td><td><a id = "value" href="OtherDegrees2.html">$200</a></td>
-    </tr>
-    <tr>
-        <td><a id = "value" href="food3.html">$300</a></td><td><a id = "value" href="buildings3.html">$300</a></td><td><a id = "value" href="compSci3.html">$300</a></td><td><a id = "value" href="parking3.html">$300</a></td><td><a id = "value" href="OtherDegrees3.html">$300</a></td>
-    </tr>
-    <tr>
-        <td><a id = "value" href="food4.html">$400</a></td><td><a id = "value" href="buildings4.html">$400</a></td><td><a id = "value" href="compSci4.html">$400</a></td><td><a id = "value" href="parking4.html">$400</a></td><td><a id = "value" href="OtherDegrees4.html">$400</a></td>
-    </tr>
-    <tr>
-        <td><a id = "value" href="food5.html">$500</a></td><td><a id = "value" href="buildings5.html">$500</a></td><td><a id = "value" href="compSci5.html">$500</a></td><td><a id = "value" href="parking5.html">$500</a></td><td><a id = "value" href="OtherDegrees5.html">$500</a></td>
-    </tr>
-</table>
 
-
-<div class="game-status">
-    <h1>IN GAME</h1>
+    <div class = game>
+        <h1>JEOPARDY</h1>
         <a href="logout.php">Click here</a> to Logout.
         <?php echo $message; ?>
-</div>
+    </div>
+
+
+    <table align ="center" border ="1" width="600" height="450">
+        <tr>
+            <td>Food</td><td>Buildings</td><td>Comp Sci</td><td>Parking</td><td>Other Degrees</td>
+        </tr>
+        <tr>
+            <td><a id = "value" href="food1.html">$100</a></td><td><a id = "value" href="buildings1.html">$100</a></td><td><a id = "value" href="compSci1.html">$100</a></td><td><a id = "value" href="parking1.html">$100</a></td><td><a id = "value" href="otherDegrees1.html">$100</a></td>
+        </tr>
+        <tr>
+            <td><a id = "value" href="food2.html">$200</a></td><td><a id = "value" href="buildings2.html">$200</a></td><td><a id = "value" href="compSci2.html">$200</a></td><td><a id = "value" href="parking2.html">$200</a></td><td><a id = "value" href="OtherDegrees2.html">$200</a></td>
+        </tr>
+        <tr>
+            <td><a id = "value" href="food3.html">$300</a></td><td><a id = "value" href="buildings3.html">$300</a></td><td><a id = "value" href="compSci3.html">$300</a></td><td><a id = "value" href="parking3.html">$300</a></td><td><a id = "value" href="OtherDegrees3.html">$300</a></td>
+        </tr>
+        <tr>
+            <td><a id = "value" href="food4.html">$400</a></td><td><a id = "value" href="buildings4.html">$400</a></td><td><a id = "value" href="compSci4.html">$400</a></td><td><a id = "value" href="parking4.html">$400</a></td><td><a id = "value" href="OtherDegrees4.html">$400</a></td>
+        </tr>
+        <tr>
+            <td><a id = "value" href="food5.html">$500</a></td><td><a id = "value" href="buildings5.html">$500</a></td><td><a id = "value" href="compSci5.html">$500</a></td><td><a id = "value" href="parking5.html">$500</a></td><td><a id = "value" href="OtherDegrees5.html">$500</a></td>
+        </tr>
+    </table>
+
+
 
 </body>
 </html>
